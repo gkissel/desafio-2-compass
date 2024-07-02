c42d5b75e994874681d92095fc14a610281b782d
Gustavo Kissel <kisselgustavo@gmail.com>
2024-07-02 15:52:20 -0300

    feat : add search movie provider

---
+++ src/api/services/movie/search-movie.provider.ts
@@ -0,0 +1,53 @@
+import { ResourceNotFoundError } from '@/api/errors/resource-not-found.error'
+import { MovieRepository } from '@/api/repositories/MovieRepository'
+import z from 'zod'
+
+export const searchMovieRequestSchema = z.object({
+  id: z.coerce.number(),
+})
+
+export const searchMovieResponseSchema = z.object({
+  id: z.number(),
+  image: z.string(),
+  name: z.string(),
+  description: z.string(),
+  actors: z.array(z.string()),
+  genre: z.string(),
+  release_date: z.string(),
+  // sessions: z.array(
+  //   z.object({
+  //     id: z.number(),
+  //     movie_id: z.number(),
+  //     room: z.string(),
+  //     capacity: z.number(),
+  //     day: z.string(),
+  //     time: z.string(),
+  //     tickets: z.array(
+  //       z.object({
+  //         id: z.number(),
+  //         session_id: z.number(),
+  //         chair: z.string(),
+  //         value: z.number(),
+  //       }),
+  //     ),
+  //   }),
+  // ),
+})
+
+type searchMovieRequestData = z.infer<typeof searchMovieRequestSchema>
+
+type searchMovieResponseData = z.infer<typeof searchMovieResponseSchema>
+
+export const SearchMovie = async ({
+  id,
+}: searchMovieRequestData): Promise<searchMovieResponseData> => {
+  // eslint-disable-next-line camelcase
+  const movies = await MovieRepository.find({
+    where: { id },
+  })
+  if (!movies) {
+    throw new ResourceNotFoundError()
+  }
+
+  return searchMovieResponseSchema.parse(movies)
+}
