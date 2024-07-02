87159b66fc1e83df9566ed1b6a0df5aabc4168e2
Gustavo Kissel <kisselgustavo@gmail.com>
2024-07-02 15:51:57 -0300

    feat : add create movie provider

---
+++ src/api/services/movie/create-movie.provider.ts
@@ -0,0 +1,49 @@
+import { MovieRepository } from '@/api/repositories/MovieRepository'
+import z from 'zod'
+
+export const createMovieRequestSchema = z.object({
+  image: z.string(),
+  name: z.string(),
+  description: z.string(),
+  actors: z.array(z.string()),
+  genre: z.string(),
+  release_date: z.string(),
+})
+
+const createMovieResponseSchema = z.object({
+  id: z.number(),
+  image: z.string(),
+  name: z.string(),
+  description: z.string(),
+  actors: z.array(z.string()),
+  genre: z.string(),
+  release_date: z.string(),
+})
+
+type createMovieRequestData = z.infer<typeof createMovieRequestSchema>
+
+type createMovieResponseData = z.infer<typeof createMovieRequestSchema>
+
+export const CreateMovie = async (
+  movieData: createMovieRequestData,
+): Promise<createMovieResponseData> => {
+  // eslint-disable-next-line camelcase
+  const { actors, description, genre, image, name, release_date } =
+    createMovieRequestSchema.parse(movieData)
+
+  if (await MovieRepository.exists({ where: { name } })) {
+    throw new Error('Movie name already registered')
+  }
+  const movie = MovieRepository.create({
+    actors,
+    description,
+    genre,
+    image,
+    name,
+    release_date: new Date(release_date).toISOString(),
+  })
+
+  await MovieRepository.save(movie)
+
+  return createMovieResponseSchema.parse(movie)
+}
