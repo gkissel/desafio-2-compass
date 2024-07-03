94a53f4097c4acba60bece9a8115a8e3ddfee899
Ana <anaclaraqb@gmail.com>
2024-07-03 13:59:26 -0300

    feat: create movies table migration

---
+++ src/database/migrations/1720024105507-createMoviesTable.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateMoviesTable1720024105507 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'movies',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'image',
+            type: 'varchar',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'actors',
+            type: 'varchar',
+            isArray: true,
+          },
+          {
+            name: 'genre',
+            type: 'varchar',
+          },
+          {
+            name: 'release_date',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('movies');
+  }
+}
