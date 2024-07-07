f7a6531106da78d5c583a2105750e0c2a178e5b8
Ana <anaclaraqb@gmail.com>
2024-07-07 20:40:07 -0300

    fix: list movies return

--- src/api/services/movies/list-movie.provider.ts
+++ src/api/services/movies/list-movie.provider.ts
@@ -1,7 +1,9 @@
-import { ResourceNotFoundError } from '@/api/errors/resource-not-found.error'
-import { MovieRepository } from '@/api/repositories/MovieRepository'
-import { listMovieSchema } from '@/api/schemas/movie.schemas'
-import { listMovieData, listPaginationData } from '@/api/types/movie.types'
+import { ResourceNotFoundError } from '@/api/errors/resource-not-found.error';
+import { MovieRepository } from '@/api/repositories/MovieRepository';
+import { SessionRepository } from '@/api/repositories/SessionRepository';
+import { TicketRepository } from '@/api/repositories/TicketRepository';
+import { listMovieSchema } from '@/api/schemas/movie.schemas';
+import { listMovieData, listPaginationData } from '@/api/types/movie.types';
 
 export const ListMovie = async ({
   page,
@@ -11,10 +13,23 @@ export const ListMovie = async ({
   const movies = await MovieRepository.find({
     take: perPage,
     skip: (page - 1) * perPage,
-  })
+  });
   if (!movies) {
-    throw new ResourceNotFoundError()
+    throw new ResourceNotFoundError();
   }
 
-  return listMovieSchema.parse(movies)
-}
+  for (let i = 0; i < movies.length; i++) {
+    let sessions = await SessionRepository.find({
+      where: { movie_id: movies[i].id },
+    });
+    for (let i = 0; i < sessions.length; i++) {
+      const tickets = await TicketRepository.find({
+        where: { session_id: sessions[i].id },
+      });
+      sessions[i].tickets = tickets;
+    }
+    movies[i].sessions = sessions;
+  }
+
+  return listMovieSchema.parse(movies);
+};
